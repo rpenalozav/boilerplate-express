01de31b6aff0e5e31f38aaa9bb34aa205707bc90
ricardo <rpenalozav@unal.edu.co>
2021-05-16 22:05:08 -0500

    add file static css

--- myApp.js
+++ myApp.js
@@ -2,42 +2,13 @@ var express = require('express');
 var app = express();
 console.log("Hello World")
 
+app.use('/public', express.static(__dirname + '/public'))
 absolutePath = __dirname + '/views/index.html'
 
-app.get('/', function (req,res){
- res.sendFile(absolutePath)
+app.get('/', function (req, res) {
+    res.sendFile(absolutePath)
+    console.log(res)
 })
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
